ce6df1935c281967ea9f96398f896ea1c80a935a
fmacgyve <gleb.andreev2000@gmail.com>
2019-03-28 22:49:42 +0300

    Сделал столкновения с маленькими стенами

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 17:06:35 by fmacgyve          #+#    #+#             */
-/*   Updated: 2019/03/28 16:41:45 by marvin           ###   ########.fr       */
+/*   Updated: 2019/03/28 22:48:26 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@
 void	move_forward(t_doom **doom)
 {
 	if ((*doom)->map[(int)(*doom)->pos.y]
-		[(int)((*doom)->pos.x + (*doom)->dir.x * MOVE_SPEED)]->height != 1.0)
+		[(int)((*doom)->pos.x + (*doom)->dir.x * MOVE_SPEED)]->height == 0.0)
 		(*doom)->pos.x += (*doom)->dir.x * MOVE_SPEED;
 	if ((*doom)->map[(int)((*doom)->pos.y + (*doom)->dir.y * MOVE_SPEED)]
-				[(int)((*doom)->pos.x)]->height != 1.0)
+				[(int)((*doom)->pos.x)]->height == 0.0)
 		(*doom)->pos.y += (*doom)->dir.y * MOVE_SPEED;
 }
 
 void	move_back(t_doom **doom)
 {
 	if ((*doom)->map[(int)(*doom)->pos.y]
-		[(int)((*doom)->pos.x - (*doom)->dir.x * MOVE_SPEED)]->height != 1.0)
+		[(int)((*doom)->pos.x - (*doom)->dir.x * MOVE_SPEED)]->height == 0.0)
 		(*doom)->pos.x -= (*doom)->dir.x * MOVE_SPEED;
 	if ((*doom)->map[(int)((*doom)->pos.y - (*doom)->dir.y * MOVE_SPEED)]
-				[(int)((*doom)->pos.x)]->height != 1.0)
+				[(int)((*doom)->pos.x)]->height == 0.0)
 		(*doom)->pos.y -= (*doom)->dir.y * MOVE_SPEED;
 }
